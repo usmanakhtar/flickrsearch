42a93e8c5fc57831cf38678f1f1c394828737e9d
usmanakhtar <uapassion@gmail.com>
2014-12-17 11:44:33 -0800

    <Message>

---
+++ README.md
@@ -0,0 +1,13 @@
+# About Flickr Search 
+
+Single page app implemented using a HTML5 (FlexBox), CSS3, JavaScripts. Jquery along 
+with the Json features are being utilized to fetch results from Flickr Api. 
+
+## Usage
+
+The interface is simple just type the keywords and then press the submit button. Make sure you are connected 
+to internet during search. I have placed the comments on the recessary codes. 
+
+## How to delopy
+
+If you want to get a copy of an existing Git repository the command you need is git clone [url]. 
---
+++ js/script.js
@@ -0,0 +1,19 @@
+// show picture function
+function showpics(){
+
+    var pic= $("#box").val();
+    $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?tags='
+	  +pic+"&tagmode=any&format=json&jsoncallback=?",
+
+	  function(data){ $("#images").hide().html(data).fadeIn('fast');
+
+    $.each(data.items, function(i,item) {
+     $("<img/>").attr("src", item.media.m).appendTo("#images").wrap("<a href=" + item.link + " title=" + item.title + " class=lazy></a>");
+	  if ( i >= 49 ) return false;
+   });
+  })
+};
+
+
+
+
